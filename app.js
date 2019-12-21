c1029816df20d63b4b6024b6c1ae4e7148509eb6
Kater Akeren <akerenkater@gmail.com>
2019-12-21 16:30:45 +0100

    Handle Errors & uses morgan for logging incoming requests

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 /*
  ** imported the apis from the api>routes directory 
  */
@@ -11,9 +11,33 @@ const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 /*
+ ** Logging incoming requests
+ */
+app.use(morgan('dev'));
+
+
+/*
  ** Routes middleware
  */
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+/*
+ ** Handling errors
+ */
+app.use((req, res, next) => {
+    const error = new Error('oops! - Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 
 module.exports = { app }
